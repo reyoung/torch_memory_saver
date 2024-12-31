48f3e247d91c1096ee3bbd72d14e47236802c214
fzyzcjy <ch271828n@outlook.com>
2024-12-31 10:31:16 +0800

    more

--- torch_memory_saver/__init__.py
+++ torch_memory_saver/__init__.py
@@ -2,6 +2,7 @@ import ctypes
 import logging
 import os
 from contextlib import contextmanager
+from dataclasses import dataclass
 from pathlib import Path
 from typing import Optional
 
@@ -13,62 +14,61 @@ logger = logging.getLogger(__name__)
 class TorchMemorySaver:
     def __init__(self):
         self._mem_pool = torch.cuda.MemPool()
-        self._id = _global_info().next_id()
+        self._id = _global_info.next_id()
         assert self._id == 1, 'Only support one single instance yet (multi-instance will be implemented later)'
 
     @contextmanager
     def region(self):
         with torch.cuda.use_mem_pool(self._mem_pool):
-            _global_info().cdll.tms_region_enter()
+            _global_info.binary_info.cdll.tms_region_enter()
             try:
                 yield
             finally:
-                _global_info().cdll.tms_region_leave()
+                _global_info.binary_info.cdll.tms_region_leave()
 
     def pause(self):
-        _global_info().cdll.tms_pause()
+        _global_info.binary_info.cdll.tms_pause()
 
     def resume(self):
-        _global_info().cdll.tms_resume()
+        _global_info.binary_info.cdll.tms_resume()
 
 
-def _compute_cdll():
-    env_ld_preload = os.environ.get('LD_PRELOAD', '')
-    if 'torch_memory_saver' in env_ld_preload:
-        return ctypes.CDLL(env_ld_preload)
-    else:
-        logger.warning(
-            f'TorchMemorySaver is disabled for the current process because invalid LD_PRELOAD="{env_ld_preload}" (process_id={os.getpid()})')
-        return None
-
-
-class _GlobalInfo:
-    def __init__(self):
-        self.cdll: Optional[ctypes.CDLL] = _compute_cdll()
-        self._last_id = 0
-        logger.debug(f'Use cdll={self.cdll}')
+@dataclass
+class _BinaryInfo:
+    cdll: Optional[ctypes.CDLL]
 
     @property
     def enabled(self):
         return self.cdll is not None
+
+    @staticmethod
+    def compute():
+        env_ld_preload = os.environ.get('LD_PRELOAD', '')
+        if 'torch_memory_saver' in env_ld_preload:
+            return _BinaryInfo(cdll=ctypes.CDLL(env_ld_preload))
+        else:
+            logger.warning(
+                f'TorchMemorySaver is disabled for the current process because invalid LD_PRELOAD="{env_ld_preload}" (process_id={os.getpid()})')
+            return _BinaryInfo(cdll=None)
+
+
+class _GlobalInfo:
+    def __init__(self):
+        self._binary_info: Optional[_BinaryInfo] = None
+        self._last_id = 0
+
+    @property
+    def binary_info(self):
+        if self._binary_info is None:
+            self._binary_info = _BinaryInfo.compute()
+        return self._binary_info
 
     def next_id(self):
         self._last_id += 1
         return self._last_id
 
 
-class Lazy:
-    def __init__(self, creator):
-        self._creator = creator
-        self._value = None
-
-    def __call__(self):
-        if self._value is None:
-            self._value = self._creator()
-        return self._value
-
-
-_global_info = Lazy(_GlobalInfo)
+_global_info = _GlobalInfo()
 
 
 def get_binary_path():

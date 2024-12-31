699ce3302b3b172753256e3593a9ac9cf6e477e8
fzyzcjy <ch271828n@outlook.com>
2024-12-31 09:34:24 +0800

    more

--- setup.py
+++ setup.py
@@ -1,19 +1,35 @@
-from setuptools import setup
-from torch.utils import cpp_extension
+import logging
 
-ext_module = cpp_extension.CppExtension(
-    'torch_memory_saver_cpp',
-    ['csrc/torch_memory_saver.cpp'],
-    extra_compile_args=['-I/usr/local/cuda/include'],
-    extra_link_args=['-lcuda'],
-)
+from setuptools import setup
+
+logger = logging.getLogger(__name__)
+
+try:
+    from torch.utils import cpp_extension
+
+    has_torch = True
+except ImportError:
+    logger.warning('setup.py fail to import torch, thus do not build C++ modules')
+    has_torch = False
+
+if has_torch:
+    # https://pytorch.org/tutorials/advanced/cpp_extension.html#writing-a-c-extension
+    ext_modules = [cpp_extension.CppExtension(
+        'torch_memory_saver_cpp',
+        ['csrc/torch_memory_saver.cpp'],
+        extra_compile_args=['-I/usr/local/cuda/include'],
+        extra_link_args=['-lcuda'],
+    )]
+    cmdclass = {'build_ext': cpp_extension.BuildExtension}
+else:
+    ext_modules = []
+    cmdclass = {}
 
 setup(
     name='torch_memory_saver',
     version='0.0.1',
-    # https://pytorch.org/tutorials/advanced/cpp_extension.html#writing-a-c-extension
-    ext_modules=[ext_module],
-    cmdclass={'build_ext': cpp_extension.BuildExtension},
+    ext_modules=ext_modules,
+    cmdclass=cmdclass,
     python_requires=">=3.9",
     packages=['torch_memory_saver'],
 )

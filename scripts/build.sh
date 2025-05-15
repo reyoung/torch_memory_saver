7fe6a1f592705c560e4d805d2c92bb415d2e2caa
fzyzcjy <ch271828n@outlook.com>
2025-05-15 09:39:07 +0800

    more

--- scripts/build.sh
+++ scripts/build.sh
@@ -38,17 +38,11 @@ docker run --rm \
    -v $(pwd):/app \
    ${DOCKER_IMAGE} \
    bash -c "
-   set -euxo pipefail && \
-
-   ${PYTHON_ROOT_PATH}/bin/${TORCH_INSTALL} && \
-   ${PYTHON_ROOT_PATH}/bin/pip install --no-cache-dir ninja setuptools==75.0.0 wheel==0.41.0 numpy uv scikit-build-core && \
-   export TORCH_CUDA_ARCH_LIST='7.5 8.0 8.9 9.0+PTX' && \
-   export CUDA_VERSION=${CUDA_VERSION} && \
-   mkdir -p /usr/lib/${ARCH}-linux-gnu/ && \
-   ln -s /usr/local/cuda-${CUDA_VERSION}/targets/${LIBCUDA_ARCH}-linux/lib/stubs/libcuda.so /usr/lib/${ARCH}-linux-gnu/libcuda.so && \
-
-   cd /app && \
-   ls -la ${PYTHON_ROOT_PATH}/lib/python${PYTHON_VERSION}/site-packages/wheel/ && \
-   PYTHONPATH=${PYTHON_ROOT_PATH}/lib/python${PYTHON_VERSION}/site-packages ${PYTHON_ROOT_PATH}/bin/python -m uv build --wheel -Cbuild-dir=build . --color=always --no-build-isolation && \
-   ./rename_wheels.sh
+   PYTHON_ROOT_PATH=${PYTHON_ROOT_PATH} \
+   PYTHON_VERSION=${PYTHON_VERSION} \
+   TORCH_INSTALL=${TORCH_INSTALL} \
+   CUDA_VERSION=${CUDA_VERSION} \
+   ARCH=${ARCH} \
+   LIBCUDA_ARCH=${LIBCUDA_ARCH} \
+   ./build_in_docker.sh
    "

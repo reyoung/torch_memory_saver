7d3e5e87a995e69da507da5dae6091bcd91dacf7
Johnny <johnnync13@gmail.com>
2025-06-04 19:41:28 +0200

    Update build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -6,19 +6,28 @@ set -euxo pipefail
 echo "python=${PYTHON_VERSION} cuda=${CUDA_VERSION}" # args
 
 PYTHON_ROOT_PATH=/opt/python/cp${PYTHON_VERSION//.}-cp${PYTHON_VERSION//.}
-ARCH=$(uname -i)
-
-echo "ARCH:  $ARCH"
-if [ ${ARCH} = "aarch64" ]; then
-   LIBCUDA_ARCH="sbsa"
-   BUILDER_NAME="pytorch/manylinuxaarch64-builder"
-else
-   LIBCUDA_ARCH=${ARCH}
-   if [ ${CUDA_VERSION} = "12.8" ]; then
+ARCH=$(uname -m)
+TEGRA="tegra"
+if [ -z "${SYSTEM_ARCH}" ]; then
+  echo "ARCH:  $ARCH"
+  if [ "$ARCH" = "aarch64" ]; then
+    echo "### ARM64 architecture detected"
+    if uname -a | grep -qi "$TEGRA"; then
+      echo "### Jetson Detected"
+      LIBCUDA_ARCH="aarch64"
+      BUILDER_NAME="pytorch/manylinuxaarch64-builder"
+    else
+      echo "### SBSA Detected"
+      LIBCUDA_ARCH="sbsa"
+      BUILDER_NAME="pytorch/manylinuxaarch64-builder"
+    fi
+  else
+    echo "### x86 Detected"
+    if [ ${CUDA_VERSION} = "12.8" ]; then
       BUILDER_NAME="pytorch/manylinux2_28-builder"
    else
       BUILDER_NAME="pytorch/manylinux-builder"
-   fi
+  fi
 fi
 
 DOCKER_IMAGE="${BUILDER_NAME}:cuda${CUDA_VERSION}"

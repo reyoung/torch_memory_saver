8cb1badbce33cfc1a615e4ed617c871554304e81
Johnny <johnnync13@gmail.com>
2025-06-05 15:09:21 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -26,17 +26,9 @@ def _find_cuda_home():
 
 
 cuda_home = Path(_find_cuda_home())
-arch = platform.machine()
-
-if arch == 'aarch64':
-    target_dir = 'targets/aarch64-linux'
-else:
-    target_dir = 'targets/x86_64-linux'
-
 include_dirs = [
-    str(cuda_home.resolve() / target_dir / 'include'),
+    str(cuda_home.resolve() / 'targets/x86_64-linux/include'),
 ]
-
 library_dirs = [
     str(cuda_home.resolve() / 'lib64'),
     str(cuda_home.resolve() / 'lib64/stubs'),

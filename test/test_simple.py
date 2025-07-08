571add6d7505f5d79a5f738e9e9ceb5d19e13b7f
fzyzcjy <ch271828n@outlook.com>
2025-07-08 14:47:41 +0800

    more

--- test/test_simple.py
+++ test/test_simple.py
@@ -1,1 +1,13 @@
-def test_simple:
+from test.utils import run_in_subprocess
+
+
+def _test_simple_inner():
+    import torch
+    a = torch.tensor([42], device="cuda")
+    b = a * 2
+    print(f"{a=} {b=}")
+    assert b.item() == 84
+
+
+def test_simple():
+    run_in_subprocess(_test_simple_inner)

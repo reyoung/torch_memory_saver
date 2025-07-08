927a141009c84f0c2f435075726e4417a273b49d
fzyzcjy <ch271828n@outlook.com>
2025-07-08 16:53:11 +0800

    more

--- test/test_simple.py
+++ test/test_simple.py
@@ -1,3 +1,11 @@
-@pytest.mark.parametrize("hook_mode", ["preload", "torch"])
+import pytest
+
+from test.utils import configure_tms_and_run_in_subprocess
+from .examples import simple
+
+_HOOK_MODES = ["preload", "torch"]
+
+
+@pytest.mark.parametrize("hook_mode", _HOOK_MODES)
 def test_simple(hook_mode):
-    configure_tms_and_run_in_subprocess(_test_simple_inner, hook_mode=hook_mode)
+    configure_tms_and_run_in_subprocess(simple.run, hook_mode=hook_mode)

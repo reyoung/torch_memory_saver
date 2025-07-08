677f968c7d030da7c84a9a573adcca0dd70b05dd
fzyzcjy <ch271828n@outlook.com>
2025-07-08 15:31:54 +0800

    more

--- torch_memory_saver/hooks/mode_torch.py
+++ torch_memory_saver/hooks/mode_torch.py
@@ -8,4 +8,5 @@ class HookUtilModeTorch(HookUtilBase):
         return str(get_binary_path_from_package("torch_memory_saver_hook_mode_torch"))
 
     def create_allocator(self):
-        return CUDAPluggableAllocator(self.get_path_binary(), "tms_torch_malloc", "tms_torch_free")
+        allocator = CUDAPluggableAllocator(self.get_path_binary(), "tms_torch_malloc", "tms_torch_free")
+        return allocator.allocator()

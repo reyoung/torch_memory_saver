97f184248648cf1a88c96392ce0ba9d9fb0705f8
fzyzcjy <ch271828n@outlook.com>
2025-07-08 14:49:38 +0800

    more

--- test/utils.py
+++ test/utils.py
@@ -13,10 +13,12 @@ def run_in_subprocess(fn):
 
 def _subprocess_fn_wrapper(fn, output_queue):
     try:
+        print(f"Subprocess execution start")
         fn()
+        print(f"Subprocess execution end", flush=True)
         output_queue.put(True)
     except Exception as e:
-        print(f"subprocess has error: {e}", flush=True)
+        print(f"Subprocess has error: {e}", flush=True)
         traceback.print_exc()
         output_queue.put(False)
         raise

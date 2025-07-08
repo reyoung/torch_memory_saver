2418de0d3205a30b86fb7d4845ea9bf26495a3ed
fzyzcjy <ch271828n@outlook.com>
2025-07-08 11:57:45 +0800

    more

--- torch_memory_saver/__init__.py
+++ torch_memory_saver/__init__.py
@@ -112,35 +112,3 @@ _global_info = _GlobalInfo()
 # Global singleton instance
 torch_memory_saver = TorchMemorySaver()
 
-
-@contextmanager
-def configure_subprocess():
-    with _change_env("LD_PRELOAD", str(_get_binary_path())):
-        yield
-
-
-def _get_binary_path():
-    dir_package = Path(__file__).parent
-    TODO_handle_so_postfix
-    candidates = [
-        p
-        for d in [dir_package, dir_package.parent]
-        for p in d.glob("torch_memory_saver_cpp.*.so")
-    ]
-    assert (
-            len(candidates) == 1
-    ), f"Expected exactly one torch_memory_saver_cpp library, found: {candidates}"
-    return candidates[0]
-
-
-@contextmanager
-def _change_env(key: str, value: str):
-    old_value = os.environ.get(key, "")
-    os.environ[key] = value
-    logger.debug(f"change_env set key={key} value={value}")
-    try:
-        yield
-    finally:
-        assert os.environ[key] == value
-        os.environ[key] = old_value
-        logger.debug(f"change_env restore key={key} value={old_value}")

07f1922c72d8141a43cec5575816b12b752642d5
fzyzcjy <ch271828n@outlook.com>
2025-07-08 12:01:10 +0800

    more

--- torch_memory_saver/binary_wrapper.py
+++ torch_memory_saver/binary_wrapper.py
@@ -21,6 +21,7 @@ class BinaryWrapper:
 
     @staticmethod
     def compute():
+        TODO_refactor_this
         env_ld_preload = os.environ.get("LD_PRELOAD", "")
         if "torch_memory_saver" in env_ld_preload:
             try:

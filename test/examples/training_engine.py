75a649047da59edfc2e31f05389d4a1349a9a267
fzyzcjy <ch271828n@outlook.com>
2025-08-12 08:16:15 +0800

    more

--- test/examples/training_engine.py
+++ test/examples/training_engine.py
@@ -1,6 +1,10 @@
 import logging
 import os
 import sys
+from functools import reduce
+from typing import List
+
+import torch
 from torch_memory_saver import torch_memory_saver
 from torch_memory_saver.testing_utils import get_and_print_gpu_memory
 
@@ -12,7 +16,25 @@ def run(hook_mode: str):
     torch_memory_saver.hook_mode = hook_mode
     logging.basicConfig(level=logging.DEBUG, stream=sys.stdout)
 
+    initial_tensor = torch.full((1_000_000,), 42, dtype=torch.uint8, device='cuda')
+    mem_initial = get_and_print_gpu_memory("Init")
+
+    model_weights = [
+        torch.full((size,), 42, dtype=torch.uint8, device="cuda")
+        for size in [1024 ** 3, 1024 ** 2, 1024 ** 1, 42]
+    ]
+
+    _execute_forward_pass_and_assert(model_weights)
+
     TODO
+
+
+def _execute_forward_pass_and_assert(model_weights: List[torch.Tensor]):
+    ones = torch.ones((1024 ** 3,), dtype=torch.float32, device="cuda")
+    sum_avg_weights = reduce(lambda a, b: a + b, [w.float().mean() for w in model_weights])
+    outs = ones * sum_avg_weights
+    out = outs.mean().item()
+    assert out == 42, f"{out=}"
 
 
 if __name__ == '__main__':
